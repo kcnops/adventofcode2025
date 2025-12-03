a784aa1edbfea92d263d21b85cd19d618bb3078b
Kristof <kristof.cnops@gmail.com>
2025-12-03 19:35:57 +0100

    day 3 p2

--- day3/batteries.py
+++ day3/batteries.py
@@ -18,10 +18,26 @@ input_file = this_dir / 'input.txt'
 with input_file.open('r') as file:
     lines = file.readlines()
 
+joltage_length = 12
+
+def get_largest_index_and_digit(int_list: List[int]) -> (int, int):
+    largest = 0
+    largest_idx = None
+    for idx, digit in enumerate(int_list):
+        if digit > largest:
+            largest = digit
+            largest_idx = idx
+    if largest_idx is None:
+        raise ValueError('Did not find any battery above 0 or algorithm error.')
+    return largest_idx, largest
+
 total_joltage = 0
 for line in lines:
     line = line.strip()
+    len_line = len(line)
+    line = [int(x) for x in line]
     print(line)
+    print(f'len line: {len_line}')
 
     # largest_joltage = 0
     # for idx, battery in enumerate(line):
@@ -33,23 +49,42 @@ for line in lines:
     # print(largest_joltage)
     # total_joltage += largest_joltage
 
-    largest_leading = 0
-    largest_leading_idx = None
-    for idx, battery in enumerate(line[:len(line)-1]):
-        if int(battery) > largest_leading:
-            largest_leading = int(battery)
-            largest_leading_idx = idx
-    # print(f'{largest_leading} @ {largest_leading_idx}')
+    # largest_leading = 0
+    # largest_leading_idx = None
+    # for idx, battery in enumerate(line[:len(line) - 1]):
+    #     if int(battery) > largest_leading:
+    #         largest_leading = int(battery)
+    #         largest_leading_idx = idx
+    # # print(f'{largest_leading} @ {largest_leading_idx}')
+    #
+    # if largest_leading_idx is None:
+    #     raise ValueError('Did not find any battery above 0 or algorithm error.')
+    #
+    # largest_trailing = 0
+    # for battery in line[largest_leading_idx + 1:]:
+    #     if int(battery) > largest_trailing:
+    #         largest_trailing = int(battery)
+    # # print(f'{largest_trailing}')
+    #
+    # total_joltage += 10 * largest_leading + largest_trailing
 
-    if largest_leading_idx is None:
-        raise ValueError('Did not find any battery above 0 or algorithm error.')
+    joltage = 0
+    next_idx = 0
+    for i in range(joltage_length):
+        # i is how many digits already found
+        print(f'i: {i}')
+        len_still_needed = len_line - joltage_length + i + 1
+        print(f'len still needed: {len_still_needed}')
+        line_to_look_in = line[next_idx:len_still_needed]
+        print(line_to_look_in)
+        relative_idx, largest = get_largest_index_and_digit(line_to_look_in)
+        next_idx = next_idx + relative_idx + 1
+        joltage = 10 * joltage + largest
+        print(f'{joltage}')
+    print(f'{joltage}')
 
-    largest_trailing = 0
-    for battery in line[largest_leading_idx+1:]:
-        if int(battery) > largest_trailing:
-            largest_trailing = int(battery)
-    # print(f'{largest_trailing}')
-
-    total_joltage += 10*largest_leading + largest_trailing
+    total_joltage += joltage
 
 print(total_joltage)
+
+# 169732779262668 is too high
